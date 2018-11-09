0d50ae37a656d9257b51e4a9ee554918e973a575
ngodinhloc1998 <ngodinhloc1998@gmail.com>
2018-11-10 01:00:26 +0700

    Done checkout

---
+++ demo_v1001/application/models/Tables_model.php
@@ -0,0 +1,62 @@
+<?php
+    class Tables_model extends CI_Model{
+        function __construct(){
+            parent::__construct();
+            $this->load->database();
+        }
+
+        function getTables(){
+            $sql = "SELECT * FROM tables ORDER BY  LENGTH(name),CAST(name AS SIGNED) ASC,name ASC";
+            $query = $this->db->query($sql);
+            $rows = $query->result_array();
+            if(!empty($rows)){
+                return $rows;
+            }
+            return false;
+        }
+
+        function getIdTableByName($name){
+            $sql = "SELECT id FROM tables WHERE name = ?";
+            try{
+                if($query = $this->db->query($sql,$name)){
+                    echo $query->first_row();
+                    return $query->first_row();
+                }
+            }catch(Exception $e){
+                echo "Something went wrong";
+            }
+            return false;
+        }
+
+        function updateWhenTableIsOrdered($id){
+            $name_schedule = 'update_book_schedule_'.$id;
+            $sql = "DROP EVENT IF EXISTS `$name_schedule`";
+            $query = $this->db->query($sql,$id);
+            $sql = "UPDATE tables SET status = 'active' WHERE id = ?";
+            $query = $this->db->query($sql,$id);
+        }
+
+        function updateTableIsAvailable($id){
+            $name_schedule = 'update_book_schedule_'.$id;
+            $sql = "DROP EVENT IF EXISTS `$name_schedule`";
+            $query = $this->db->query($sql,$id);
+            $sql = "UPDATE tables SET status = 'available' WHERE id = ?";
+            $query = $this->db->query($sql,$id);
+        }
+
+        function updateWhenTableIsBooked($data){//data : id_table,time_booked
+            $data1 = array('booked',$data['time_booked'],$data['id_table']);
+            $sql = "UPDATE tables SET status = ?,book_at = ? WHERE id = ?";
+            if($query = $this->db->query($sql,$data1)){
+                $name_schedule = 'update_book_schedule_'.$data['id_table'];
+                $data2 = array($data['time_booked'],'available',$data['id_table']);
+                $sql = "CREATE EVENT `$name_schedule` ON SCHEDULE AT ? + INTERVAL 15 MINUTE DO UPDATE tables SET status = ? WHERE id = ?";
+                if($query = $this->db->query($sql,$data2)){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+?>
---
+++ demo_v1001/application/views/auth/registration.php
@@ -0,0 +1,177 @@
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+    <form id="registrationForm" action="<?php echo base_url('users/createAccount');?>" method="POST" class="form-group" onsubmit="return validateRegister();">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" name="name" <?php echo isset($_POST['name']) ? $_POST['name'] : ''; ?>>
+        <label for="cmnd">CMND</label>
+        <input type="text" class="form-control" id="cmnd" name="cmnd" <?php echo isset($_POST['cmnd']) ? $_POST['cmnd'] : ''; ?>>
+        <label for="username">Username</label>
+        <input type="text" class="form-control" id="username" name="username" <?php echo isset($_POST['username']) ? $_POST['username'] : ''; ?>>
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password">
+        <label for="validatePassword">Password Validate</label>
+        <input type="password" type="validatePassword" class="form-control" id="validatePassword" name="validatePassword">
+        <button class="btn btn-primary form-control" id="submit">
+            Submit
+        </button>
+    </form>
+
+    <script>
+        function validateRegister(){
+            var regisForm = document.getElementById("registrationForm");
+            var btnSubmit = document.getElementById("submit");
+                
+                if(!(regisForm['name'].value =="") && !(regisForm['cmnd'].value=="") && !(regisForm['username'].value=="") && !(regisForm['password'].value=="") && !(regisForm['validatePassword'].value == "")){
+                    if((regisForm['password'].value == regisForm['validatePassword'].value)){
+                        alert("Ok");
+                        return true;
+                    }else{
+                        alert("Xac nhan lai mat khau");
+                        return false;
+                    }
+                }
+                alert("Nhap cho dung");
+                return false;
+        }
+
+    </script>
+</body>
+</html>
+
+-->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('public/assets/images/favicon.png');?>">
+    <title>Matrix Template - The Ultimate Multipurpose admin template</title>
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url('public/dist/css/style.min.css');?>" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Login box.scss -->
+        <!-- ============================================================== -->
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center bg-dark">
+            <div class="auth-box bg-dark border-top border-secondary">
+                <div>
+                    <div class="text-center p-t-20 p-b-20">
+                        <span class="db"><img src="<?php echo base_url('public/assets/images/logo.png'); ?>" alt="logo" /></span>
+                    </div>
+                    <!-- Form -->
+                    <form class="form-horizontal m-t-20" action="index.html">
+                        <div class="row p-b-30">
+                            <div class="col-12">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-success text-white" id="basic-addon1"><i class="ti-user"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control form-control-lg" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" required>
+                                </div>
+                                <!-- email -->
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-danger text-white" id="basic-addon1"><i class="ti-email"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control form-control-lg" placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1" required>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-warning text-white" id="basic-addon2"><i class="ti-pencil"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control form-control-lg" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" required>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-info text-white" id="basic-addon2"><i class="ti-pencil"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control form-control-lg" placeholder=" Confirm Password" aria-label="Password" aria-describedby="basic-addon1" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row border-top border-secondary">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <div class="p-t-20">
+                                        <button class="btn btn-block btn-lg btn-info" type="submit">Sign Up</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Login box.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper scss in scafholding.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper scss in scafholding.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right Sidebar -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right Sidebar -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- All Required js -->
+    <!-- ============================================================== -->
+    <script src="<?php echo base_url('public/assets/libs/jquery/dist/jquery.min.js');?>"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="<?php echo base_url('public/assets/libs/popper.js/dist/umd/popper.min.js');?>"></script>
+    <script src="<?php echo base_url('public/assets/libs/bootstrap/dist/js/bootstrap.min.js');?>"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugin js -->
+    <!-- ============================================================== -->
+    <script>
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".preloader").fadeOut();
+    </script>
+</body>
+
+</html>
